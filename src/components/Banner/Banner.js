f75027ccee4a4f938394e7453fc79175bc0c4dae
DevikaRajesh22 <devikaraj699@gmail.com>
2024-01-10 11:27:59 +0530

    Banner image,title and description made dynamic using api axios

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
+import axios from '../../axios';
+import { API_KEY, imageUrl } from '../../constants/constants';
+
+const Banner = () => {
+  const [movie, setMovie] = useState();
+  const [randomNumber, setRandomNumber] = useState(0);
+  useEffect(() => {
+    setRandomNumber(Math.floor(Math.random() * 20) + 1);
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+      setMovie(response.data.results[randomNumber]);
+    })
+  },[randomNumber]);
+  return (
+    <div
+      style={{ backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})` }}
+      className='banner'>
+      <div className='content'>
+        <h1 className='title'>{movie ? movie.title : ""}</h1>
+        <div className='banner_buttons'>
+          <button className='button'>Play</button>
+          <button className='button'>My list</button>
+        </div>
+        <h1 className='description'>{movie ? movie.overview : ""}</h1>
+      </div>
+      <div className="fade_bottom"></div>
+    </div>
+  )
+}
+
+export default Banner;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='29d63a8038b82a08d1611c9203b1b6dc';
+export const imageUrl='https://image.tmdb.org/t/p/original';
