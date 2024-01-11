8f5764b0ca45a300062c85726b4a8d9b995f349d
DevikaRajesh22 <devikaraj699@gmail.com>
2024-01-11 10:36:50 +0530

    added titles

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './RowPost.css';
-import { imageUrl,API_KEY } from '../../constants/constants';
+import { imageUrl, API_KEY } from '../../constants/constants';
 import axios from '../../axios';
-import Youtube from 'react-youtube'; 
+import Youtube from 'react-youtube';
 
 const RowPost = (props) => {
   const [movies, setMovies] = useState([]);
-  const [urlId,setUrlId]=useState('');
+  const [urlId, setUrlId] = useState('');
   useEffect(() => {
     axios.get(props.url).then(response => {
       setMovies(response.data.results);
     }).catch(err => {
-      //alert('network error');
+      alert('network error');
     })
   }, [])
-  const opts={
-    height:'390',
-    width:'100%',
-    playerVars:{
-      autoplay:1,
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
     },
   };
-  const handleMovie=(id)=>{
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-      if(response.data.results.length!==0){
+  const handleMovie = (id) => {
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
+      if (response.data.results.length !== 0) {
         setUrlId(response.data.results[0]);
       }
     })
@@ -33,10 +33,15 @@ const RowPost = (props) => {
       <h2>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj) =>
-          <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
+          <>
+            <div className='groupCard'>
+              <img onClick={() => handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
+              <p>{obj.title}</p>
+            </div>
+          </>
         )}
       </div>
-      {urlId && <Youtube videoId={urlId.key} opts={opts}/>}
+      {urlId && <Youtube videoId={urlId.key} opts={opts} />}
     </div>
   )
 }
